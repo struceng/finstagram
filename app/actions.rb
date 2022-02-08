b58bf9186539c2e4735dd50f34a8baa9a9b68f7c
struceng <paulehamson@gmail.com>
2022-02-08 02:54:02 +0000

    W4D1

--- app/actions.rb
+++ app/actions.rb
@@ -2,13 +2,7 @@
    ##File.read(File.join('app/views','index.html'))
 #end
 
-def humanized_time_ago(minute_num)
-    if minute_num >=60
-        "#{minute_num/60} hours ago"
-    else
-        "#{minute_num} minutes ago"
-    end
-end
+
 
 get '/' do
     @finstagram_post_shark = {
